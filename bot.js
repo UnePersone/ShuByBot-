ed73d410b2799313e7b642e5b5b3a9dc97445c1b
UnePersone <maxou3d@hotmail.fr>
2020-05-03 17:34:34 +0200

    Ajout server

--- bot.js
+++ bot.js
@@ -16,6 +16,9 @@ const tmiConfig = {
         laChaine
     ]
 };
+let express = require('express'),
+    path = require('path');
+var app = express();
 let server = require('http').Server(app);
 var port = process.env.PORT || 8000;
 server.listen(port, function() {
