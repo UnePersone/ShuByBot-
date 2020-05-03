e28982748ef1d7e0322fb92ca5f52e33ab1827fa
UnePersone <maxou3d@hotmail.fr>
2020-05-03 16:51:31 +0200

    Remise à zéro port + procfile enlever espace

--- bot.js
+++ bot.js
@@ -24,8 +24,8 @@ let client = new tmi.client(tmiConfig);
 
 client.connect();
 
-client.on('connected', (adress, '5000') => {
-    console.log(client.getUsername() + " s'est connecté sur : " + adress + ", port : " + '5000')
+client.on('connected', (adress, port) => {
+    console.log(client.getUsername() + " s'est connecté sur : " + adress + ", port : " + port)
 });
 	
 	client.on('chat',(channel, user, message, self)=>{
